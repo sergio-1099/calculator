436a723f21aa7a752102be361df5dd8464207e9b
Sergio Guerra <sergioguerra6188@gmail.com>
2022-12-23 21:17:25 -0500

    Add operator function, stop consecutive operators
    Added operator functionality using similar method to numberButtons. Also
    added a function that checks if the previous char in the expression was
    an operator to prevent two operators side by side.

--- calculator.js
+++ calculator.js
@@ -1,4 +1,5 @@
 const numberButtons = document.querySelectorAll('.operand');
+const operatorButtons = document.querySelectorAll('.operator');
 let expression = '';
 
 
@@ -9,3 +10,17 @@ for (let i = 0; i < numberButtons.length; i++) {
     });
 }
 
+for (let i = 0; i < operatorButtons.length; i++) {
+    operatorButtons[i].addEventListener('click', () => {
+        if (checkOperator(expression)) return;
+        expression += operatorButtons[i].textContent;
+        console.log(expression);
+    });
+}
+
+function checkOperator(expression) {
+    let operatorArray = ['+', '-', 'X', '/'];
+    for (const element of operatorArray) {
+        if (expression.charAt(expression.length - 1) == element) return true;
+    }
+}
