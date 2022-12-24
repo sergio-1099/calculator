6713022ad6dea65e30b0632de3e1fdb0e70856fa
Sergio Guerra <sergioguerra6188@gmail.com>
2022-12-23 21:58:48 -0500

    Add functions that solve the expression keyed in
    Added a function that separates the original expression into an array
    that contains operands and symbol. That way, the solution can be solved
    easily.
    
    Problem: Calculator cannot solve problem with more than one operator.

--- calculator.js
+++ calculator.js
@@ -2,9 +2,13 @@ const numberButtons = document.querySelectorAll('.operand');
 const operatorButtons = document.querySelectorAll('.operator');
 let calcScreen = document.querySelector('.screen');
 const clearButton = document.querySelector('.calc-clear');
+const equalsButton = document.querySelector('.calc-equals');
 let expression = '';
 
-clearButton.addEventListener('click', () => calcScreen.textContent = '');
+clearButton.addEventListener('click', () => {
+    calcScreen.textContent = ''
+    expression = '';
+});
 
 for (let i = 0; i < numberButtons.length; i++) {
     numberButtons[i].addEventListener('click', () => {
@@ -21,9 +25,53 @@ for (let i = 0; i < operatorButtons.length; i++) {
     });
 }
 
+equalsButton.addEventListener('click', function () {calcScreen.textContent = solveOperation(expression)});
+
 function checkOperator(expression) {
     let operatorArray = ['+', '-', 'X', '/'];
     for (const element of operatorArray) {
         if (expression.charAt(expression.length - 1) == element) return true;
     }
+}
+
+function solveOperation(expression) {
+    let operatorArray = ['+', '-', 'X', '/'];
+    let expressionArray = [];
+    let number;
+    for (let operator of operatorArray) {
+        for (let i = 0; i < expression.length; i++) {
+            if (operator == expression.charAt(i)) {
+                expressionArray = [expression.substring(0, i), operator, expression.substring(i+1)];
+                if (operator == '+') number = sumExp(expressionArray);
+                else if (operator == '-') number = subtractExp(expressionArray);
+                else if (operator == 'X') number = multiplyExp(expressionArray);
+                else if (operator == '/') number = divideExp(expressionArray);
+            }
+        }
+    }
+    return number;
+}
+
+function sumExp(expressionArray) {
+    let firstOperand = +expressionArray[0];
+    let secondOperand = +expressionArray[2];
+    return firstOperand + secondOperand;
+}
+
+function subtractExp(expressionArray) {
+    let firstOperand = +expressionArray[0];
+    let secondOperand = +expressionArray[2];
+    return firstOperand - secondOperand;
+}
+
+function multiplyExp(expressionArray) {
+    let firstOperand = +expressionArray[0];
+    let secondOperand = +expressionArray[2];
+    return firstOperand * secondOperand;
+}
+
+function divideExp(expressionArray) {
+    let firstOperand = +expressionArray[0];
+    let secondOperand = +expressionArray[2];
+    return firstOperand / secondOperand;
 }
